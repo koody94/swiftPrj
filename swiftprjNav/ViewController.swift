72ed3d9ed7d6bc841eba7bd0baec3c66a5b83611
doyeon koo <koody94@naver.com>
2016-05-31 14:42:38 +0900

    Initial Commit

---
+++ swiftprjNav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftprjNav
+//
+//  Created by MF839-013 on 2016. 5. 31..
+//  Copyright © 2016년 SDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
